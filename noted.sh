386c01d9e6a9a974b55d5a25237a8abfb1e7ec84
badrules <ahmad.badrul@vcgamers.com>
2022-08-16 07:36:06 +0700

    run instruction

---
+++ noted.sh
@@ -0,0 +1,8 @@
+# 31 FROM INSTRUCTION
+# example command built image /  
+❯ docker build -t dockerin635/bad badfolder
+
+
+# 32 run instruction
+# create file menggunakan docker folder
+❯ ❯ docker build -t dockerin635/run run --progres==plain --no-cache
