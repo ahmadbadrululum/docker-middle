cee941f048adb3dfae3166a23227cee2d949c3a1
badrules <ahmad.badrul@vcgamers.com>
2022-08-16 07:46:56 +0700

    command docker

--- noted.sh
+++ noted.sh
@@ -6,3 +6,16 @@
 # 32 run instruction
 # create file menggunakan docker folder
 ❯ ❯ docker build -t dockerin635/run run --progres==plain --no-cache
+
+
+# 33 command
+# CMD Instruction
+docker build -t khannedy/command command
+
+docker image inspect khannedy/command
+
+docker container create --name command khannedy/command
+
+docker container start command
+
+docker container logs command
